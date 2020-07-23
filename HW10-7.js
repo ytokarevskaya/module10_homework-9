bc173c5c6ab7cc12fab9df545fd2c11d9c03e5a6
ytokarevskaya <lan2214@yandex.ru>
2020-07-23 18:47:42 +0300

    Проверено

--- HW10-7.js
+++ HW10-7.js
@@ -9,7 +9,7 @@ for (let i=0; i<arr.length; i++) {
   if (arr[i] === 0) {
     zero = zero + 1;
   }
-  else if (isNaN(arr[i]%2) == true) {
+  else if (isNaN(arr[i]%2) || typeof arr[i] !== "number") {
     other = other + 1;
   }
   else if (arr[i] % 2 === 0) { 
@@ -19,3 +19,5 @@ for (let i=0; i<arr.length; i++) {
        }
 }
 console.log(odd, even, zero, other)
+
+// Не совсем верно, потому что при такой проверке: isNaN(arr[i]%2) == true значения типа "12" (т.е. числа, записанные строками), не будут отнесены ни в одну из категорий. Здесь лучше дополнительно использовать проверку typeof и отнести к other всё, что не является числами. == true после проверки на isNaN также можно не писать, потому что функция isNaN и так возвращает значение логического типа (true/false). Выше исправила.
